d73bf65a5dcdbae2827bd5f8bf348cc5c65d1db0
yoel-mc <yoel@mulaicoding.id>
2023-06-10 06:38:51 +0700

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Montserrat", fontFamily.sans],
+      },
+    },
+    plugins: [],
+  },
+};
